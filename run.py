5faad2a0626a8c91da7394c6d5badb029382eaa8
Rathees-Koneswarann <ratheesreo@gmail.com>
2022-10-10 18:19:26 +0530

    Plot frequency domain

--- run.py
+++ run.py
@@ -16,10 +16,13 @@ f= np.linspace(0, (N-1)*fstep, N) # freq steps
 
 y = 1*np.sin(2*np.pi*f0*t)
 
-
+# perform fft
+x = np.fft.fft(y)
+x_mag = np.abs(x) / N
 
 
 #plot
-fig, ax = plt.subplots(nrows=1, ncols=1)
-ax.plot(t,y)
+fig, [ax1, ax2] = plt.subplots(nrows=2, ncols=1)
+ax1.plot(t,y, '.-')
+ax2.plot(f,x_mag, '.-')
 plt.show()
