df1336e5ab45d42653de7309e654241e4350f40e
Rathees-Koneswarann <ratheesreo@gmail.com>
2022-10-10 21:05:46 +0530

    Open and read the wav file

---
+++ violin-audio-analyse.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import wave
+import sys
+
+
+#import audio file
+raw = wave.open("violin-C4.wav", "r")
+
+# Extract Raw Audio from Wav File
+signal = raw.readframes(-1)
+signal = np.frombuffer(signal, dtype=int)
+
+# If the audio file is Stereo exit the code
+if raw.getnchannels() == 2:
+    print("Just mono files")
+    sys.exit(0)
