3d7b507d1f28b4311a6deac494ba14a454551074
Rathees-Koneswarann <ratheesreo@gmail.com>
2022-10-11 07:23:20 +0530

    Plot the signal

--- violin-audio-analyse.py
+++ violin-audio-analyse.py
@@ -20,15 +20,21 @@ if raw.getnchannels() == 2:
 sample_rate = raw.getframerate()
 print('Sample rate of the audio is {}'.format(sample_rate))
 
-# get the number of samples
-num_of_samples = raw.getnframes()
-print('Number of samples in the audio is {}'.format(num_of_samples))
+# get the number of frames
+num_of_frames = raw.getnframes()
+print('Number of frames in the audio is {}'.format(num_of_frames))
 
 # get the duration
-duration = num_of_samples/sample_rate
+duration = num_of_frames/sample_rate
 print('Sound clip is {} seconds long'.format(duration))
+
+# develop t array to match x
+t = np.array(range(0, len(signal)))/sample_rate
 
 
 #plot the clip
-plt.plot(signal)
+plt.plot(t, signal)
+#plot the small part
+#plt.plot(t[500:750],signal[500:750])
 plt.show()
+
